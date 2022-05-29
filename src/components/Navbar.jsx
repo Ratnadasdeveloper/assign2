d28a93277b24daf0ee40c291050e1a3e13fbe489
Ratnadasdeveloper <104377250+Ratnadasdeveloper@users.noreply.github.com>
2022-05-29 14:21:26 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div class="c1">LOGOBAKERY</div>
+      <div class="c2">
+        <div>Service</div>
+        <div>Project</div>
+        <div>About</div>
+      </div>
+      <div class="c3">
+        <button>Contact</button>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
